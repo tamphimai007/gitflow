f00a6c1daa44d1c6fe0b66244611a384a48c9f42
phongphat <phongphat.jap@gmail.com>
2024-10-28 14:43:34 +0700

    update1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv");
 const app = express();
-
+console.log('tam')
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server is running on port 5000"));
